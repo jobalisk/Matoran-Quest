fa933c9c411ae0ba638bb9f4be4709fce2e6ba16
jobalisk <72062744+jobalisk@users.noreply.github.com>
2023-11-12 09:20:02 +1300

    made menus and added fonts

---
+++ Matoran Quest/Matoran Quest/MenuController.h
@@ -0,0 +1,16 @@
+//
+//  MenuController.h
+//  Matoran Quest
+//
+//  Created by Job Dyer on 12/11/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MenuController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
