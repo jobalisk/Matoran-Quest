92df37e269dcc98bb7e599376a8a0e9abc6a16d0
jobalisk <72062744+jobalisk@users.noreply.github.com>
2023-11-12 09:20:02 +1300

    made menus and added fonts

---
+++ Matoran Quest/Matoran Quest/MenuController.m
@@ -0,0 +1,31 @@
+//
+//  MenuController.m
+//  Matoran Quest
+//
+//  Created by Job Dyer on 12/11/23.
+//
+
+#import "MenuController.h"
+
+@interface MenuController ()
+
+@end
+
+@implementation MenuController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Matoran Quest/Matoran Quest/PlayerDetailsController.h
@@ -0,0 +1,16 @@
+//
+//  PlayerDetailsController.h
+//  Matoran Quest
+//
+//  Created by Job Dyer on 12/11/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlayerDetailsController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
