9e64419f5f8f6d3614024830518833e6f9bd81b3
jobalisk <72062744+jobalisk@users.noreply.github.com>
2023-11-21 08:27:21 +1300

    working on cells issue

--- Matoran Quest/Matoran Quest/UICollectionViewCell+CollectionViewCell.h
+++ Matoran Quest/Matoran Quest/UICollectionViewCell+CollectionViewCell.h
@@ -11,6 +11,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 
 @interface CollectionViewCell : UICollectionViewCell
+@property (nonatomic, strong) IBOutlet UILabel *maskName3;
+@property (nonatomic, strong) IBOutlet UIImageView *maskImage;
 
 @end
 
--- Matoran Quest/Matoran Quest/wallOfMasksController.m
+++ Matoran Quest/Matoran Quest/wallOfMasksController.m
@@ -81,13 +81,13 @@ NSMutableArray *collectedMasks2; //a list of the kinds of masks the player has c
     }
     */
     
-    UIImageView *maskImage=[[UIImageView alloc]initWithFrame:CGRectMake(18, 18, 64, 64)];
+    cell3.maskImage=[[UIImageView alloc]initWithFrame:CGRectMake(18, 18, 64, 64)];
     if(noColourFlag == 0){ //if its not a special flag, then colourize it
         UIColor *tempColor = [self colourCaser: maskColourAndName[0]]; //colour the image
         maskImage2 = [UIImage imageNamed:[NSString stringWithFormat: @"%@", maskName]];
         //[maskImage2 imageWithTintColor:tempColor];
         maskImage2 = [self colorizeImage:maskImage2 color:tempColor];
-        [maskImage setImage:maskImage2]; //set an image with a colour
+        [cell3.maskImage setImage:maskImage2]; //set an image with a colour
         [maskImage2 setAccessibilityIdentifier: maskNameAndColour];
         if(maskImage2 != nil){
             [imagesInCollection addObject: maskImage2];
@@ -98,19 +98,19 @@ NSMutableArray *collectedMasks2; //a list of the kinds of masks the player has c
         //NSLog(@"%@", imagesInCollection);
     }
     else{
-        [maskImage setImage:[UIImage imageNamed:[NSString stringWithFormat: @"%@", maskName]]]; //get the image named the name of the mask without the colour
+        [cell3.maskImage setImage:[UIImage imageNamed:[NSString stringWithFormat: @"%@", maskName]]]; //get the image named the name of the mask without the colour
     }
 
-    [cell3 addSubview:maskImage];//Add it to the view of your choice.
+    //[cell3 addSubview:cell3.maskImage];//Add it to the view of your choice.
     
-    UILabel *maskNameLabel =[[UILabel alloc]initWithFrame:CGRectMake(25, 70, 50, 50)];//Set frame of label in your view
+   cell3.maskName3 =[[UILabel alloc]initWithFrame:CGRectMake(25, 70, 50, 50)];//Set frame of label in your view
     //[itemName setBackgroundColor:[UIColor lightGrayColor]];//Set background color of label.
 
     @try {
-        [maskNameLabel setText: [NSString stringWithFormat: @"%@",maskNameAndColour]];
+        [cell3.maskName3  setText: [NSString stringWithFormat: @"%@",maskNameAndColour]];
     }
     @catch (NSException *exception) {
-        [maskNameLabel setText: @""];
+        [cell3.maskName3  setText: @""];
     }
     @finally {
       //Display Alternative
@@ -118,18 +118,18 @@ NSMutableArray *collectedMasks2; //a list of the kinds of masks the player has c
     //NSLog(@"here: %@", maskArray);
     
     //[maskName setText: @"mask"];
-    [maskNameLabel setAdjustsFontSizeToFitWidth:true];
-    [maskNameLabel setFont:[UIFont fontWithName:@"Goudy Trajan Regular" size:10]];
-    [maskNameLabel setTextColor:[UIColor whiteColor]];//Set text color in label.
-    [maskNameLabel setTextAlignment:NSTextAlignmentCenter];//Set text alignment in label.
-    [maskNameLabel setBaselineAdjustment:UIBaselineAdjustmentAlignBaselines];//Set line adjustment.
-    [maskNameLabel setLineBreakMode:NSLineBreakByCharWrapping];//Set linebreaking mode..
-    [maskNameLabel setNumberOfLines:1];//Set number of lines in label.
+    [cell3.maskName3  setAdjustsFontSizeToFitWidth:true];
+    [cell3.maskName3  setFont:[UIFont fontWithName:@"Goudy Trajan Regular" size:10]];
+    [cell3.maskName3  setTextColor:[UIColor whiteColor]];//Set text color in label.
+    [cell3.maskName3  setTextAlignment:NSTextAlignmentCenter];//Set text alignment in label.
+    [cell3.maskName3  setBaselineAdjustment:UIBaselineAdjustmentAlignBaselines];//Set line adjustment.
+    [cell3.maskName3  setLineBreakMode:NSLineBreakByCharWrapping];//Set linebreaking mode..
+    [cell3.maskName3  setNumberOfLines:1];//Set number of lines in label.
     //[itemName.layer setCornerRadius:40.0];//Set corner radius of label to change the shape.
     //[itemName.layer setBorderWidth:1.0f];//Set border width of label.
-    [maskNameLabel setClipsToBounds:YES];//Set its to YES for Corner radius to work.
-    [maskNameLabel.layer setBorderColor:[UIColor blackColor].CGColor];//Set Border color.
-    [cell3 addSubview:maskNameLabel];//Add it to the view of your choice.
+    [cell3.maskName3  setClipsToBounds:YES];//Set its to YES for Corner radius to work.
+    [cell3.maskName3 .layer setBorderColor:[UIColor blackColor].CGColor];//Set Border color.
+    //[cell3 addSubview:cell3.maskName3 ];//Add it to the view of your choice.
      
     
     
